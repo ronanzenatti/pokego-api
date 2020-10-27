11c47a32f2fc1bbf9a5a0bf362878e6a91ab809b
Claudio Onoue <co_2000@live.com>
2020-10-27 17:16:35 -0300

    Some fixes

--- docker_compose_entrypoint.sh
+++ docker_compose_entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ $API_ENV == "DEV" ]; then
+if [ $APP_ENV == "DEV" ]; then
     go get -u github.com/cosmtrek/air
 fi
 
 go mod tidy
 go mod vendor
 
-if [ $API_ENV == "DEV" ]; then
+if [ $APP_ENV == "DEV" ]; then
     air -c .air.toml
 else
     go build -o ./bin/app
